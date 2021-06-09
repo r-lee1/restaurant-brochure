a236e4b80931ed4a0f36051faeade0b77e066dbc
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-06-08 23:20:58 -0700

    add home section and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,35 @@ const scrollActive = () => {
 
 window.addEventListener('scroll', scrollActive);
 
+/* Change header on scroll greater than 200 viewport height */
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  if (this.scrollY >= 200) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+/* Show scrolltop */
+const showScrollTop = () => {
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add show-scroll class to scrollTop
+  if (this.scrollY >= 560) {
+    scrollTop.classList.add('show-scroll');
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+};
+
+window.addEventListener('scroll', showScrollTop);
+
+
+
+
+
 
 
 
