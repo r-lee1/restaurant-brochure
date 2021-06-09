5552ea0649d3f4bf82b0f552186c897a1c35ff51
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-06-08 20:56:08 -0700

    add active link on scroll and active link styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,33 @@ const linkAction = () => {
 };
 
 navLinks.forEach(n => n.addEventListener('click', linkAction));
+
+/* Active Link on Scroll*/
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach( current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
+
+
+
+
+
+
+
+
