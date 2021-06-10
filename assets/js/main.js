2286a53e29b618c5dc370192eba9c9abb275d562
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2021-06-09 18:56:54 -0700

    add our-story section and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,9 @@ const scrollActive = () => {
     const sectionId = current.getAttribute('id');
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.add('active-link');
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
     } else {
-      document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.remove('active-link');
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
     }
   });
 };
